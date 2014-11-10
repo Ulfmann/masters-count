d3c617c432426d928bdf8792af430f1ad3d80477
Ulfmann <ulf.portner@googlemail.com>
2014-11-10 22:51:51 +0100

    remove clutter

--- app/controllers/legs_controller.rb
+++ app/controllers/legs_controller.rb
@@ -1,28 +1,20 @@
 class LegsController < ApplicationController
   before_action :set_leg, only: [:show, :edit, :update, :destroy]
 
-  # GET /legs
-  # GET /legs.json
   def index
     @legs = Leg.all
   end
 
-  # GET /legs/1
-  # GET /legs/1.json
   def show
   end
 
-  # GET /legs/new
   def new
     @leg = Leg.new
   end
 
-  # GET /legs/1/edit
   def edit
   end
 
-  # POST /legs
-  # POST /legs.json
   def create
     @leg = Leg.new(leg_params)
 
@@ -37,8 +29,6 @@ class LegsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /legs/1
-  # PATCH/PUT /legs/1.json
   def update
     respond_to do |format|
       if @leg.update(leg_params)
@@ -51,8 +41,6 @@ class LegsController < ApplicationController
     end
   end
 
-  # DELETE /legs/1
-  # DELETE /legs/1.json
   def destroy
     @leg.destroy
     respond_to do |format|
@@ -62,12 +50,10 @@ class LegsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_leg
       @leg = Leg.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def leg_params
       params[:leg]
     end
